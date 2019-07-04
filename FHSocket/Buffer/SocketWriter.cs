61f8d426d42c8537748015a9694766f3e9880f1f
fuhaih <1154590036@qq.com>
2019-07-04 10:19:35 +0800

    add socketwriter

---
+++ FHSocket/Buffer/SocketWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHSocket.Buffer
+{
+    public class SocketWriter
+    {
+        public byte[] buffer;
+
+        public void Write(byte[] data)
+        {
+
+        }
+    }
+}
